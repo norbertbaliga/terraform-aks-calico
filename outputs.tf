ff48a6880e24e2545f336c81871e06de282ceada
Norbert Baliga <norbert.baliga@logmein.com>
2022-09-11 17:05:45 +0100

    Make outputs sensitive

--- outputs.tf
+++ outputs.tf
@@ -4,20 +4,25 @@ output "id" {
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.cluster.kube_config_raw
+  sensitive = true
 }
 
 output "client_key" {
   value = azurerm_kubernetes_cluster.cluster.kube_config.0.client_key
+  sensitive = true
 }
 
 output "client_certificate" {
   value = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
   value = azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate
+  sensitive = true
 }
 
 output "host" {
   value = azurerm_kubernetes_cluster.cluster.kube_config.0.host
+  sensitive = true
 }
