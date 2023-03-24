6be540c5530e549a37e4519818d1d48fe3a06f17
Norbert Baliga <norbert.baliga@logmein.com>
2023-03-24 19:39:43 +0100

    Add resources and options for AKS

--- variables.tf
+++ variables.tf
@@ -3,7 +3,46 @@ variable "prefix" {
   default     = "akscalico"
 }
 
+variable "rg_name" {
+  description = "Name of the Azure Ressource Group"
+  type        = string
+  default     = "learning-aks"
+}
+
 variable "location" {
-  description = "The Azure Region in which all resources should be provisioned"
+  description = "Location of the Azure Ressource Group"
+  type        = string
   default     = "North Europe"
+}
+
+variable "resource_tags" {
+  description = "Tags to set for all resources"
+  type        = map(string)
+  default = {
+    environment  = "dev"
+  }
+}
+
+variable "master_vm_size" {
+  description = "The VM size (SKU) for the master node."
+  type        = string
+  default     = "Standard_B2ms"
+}
+
+variable "worker_vm_size" {
+  description = "The VM size (SKU) for the worker node."
+  type        = string
+  default     = "Standard_B2ms"
+}
+
+variable "k8s_version" {
+  description = "The Kubernetes version specified for the cluster."
+  type        = string
+  default     = "1.25"
+}
+
+variable "admin_groups" {
+  description = "A list of Object IDs of Azure Active Directory Groups which should have Admin Role on the Cluster."
+  type        = list(string)
+  default     = []
 }
